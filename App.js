34d61102eab43f1f5ebb8e5080db99ae0292730a
Michael Drapeza <michael.drapeza@voypost.com>
2019-06-14 16:23:24 +0300

    TextInput and Button

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  _onPressButton() {
+    Alert.alert('You tapped the button!')
+  }
+
   render() {
     return (
       <View style={styles.container}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Type here..."
+          onChangeText={(text) => this.setState({text})}
+        />
         <Text>Open up App.js to start working on your app!</Text>
+        <Button
+            onPress={this._onPressButton}
+            title="Press me!"
+          />
       </View>
     );
   }
